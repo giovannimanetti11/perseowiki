a0447de205b0efd8784ebfae1113f26f189e82a1
root <root@vps-c45e09e8.vps.ovh.net>
2023-03-02 15:44:34 +0000

    Add menu position and search bar

--- functions.php
+++ functions.php
@@ -43,7 +43,7 @@ add_action( 'init', 'perseowiki_nav_menus' );
 
 function perseowiki_styles() {
 
-  wp_enqueue_style('perseowiki-style', get_template_directory_uri().'style.css');
+  wp_enqueue_style('perseowiki-style', get_template_directory_uri().'/style.css');
 
 }
 
--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 
 <h1><?php bloginfo('name'); ?></h1>
 
+<nav>
+    <?php
+    wp_nav_menu(array(
+        'theme_location' => 'perseowiki-primary-menu',
+        'container' => false,
+        'items_wrap' => '<ul>%3$s</ul>'
+    ));
+    ?>
+    <?php get_search_form(); ?>
+</nav>
+
+
 <?php get_footer();  ?>
