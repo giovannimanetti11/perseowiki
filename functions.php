18f2dc05aa0497e38ebfbcd95d0cc640859f0bdc
root <root@vps-c45e09e8.vps.ovh.net>
2023-03-02 14:24:02 +0000

    Added support for featured images

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@ function perseowiki_support() {
    *
    * @link https://codex.wordpress.org/Theme_Logo
    */
+
   add_theme_support( 
     'custom-logo', [
     'width'       => 180,
@@ -17,16 +18,27 @@ function perseowiki_support() {
     'flex-height' => true,
   ]);
 
+  /**
+   * Add featured images to post and pages.
+   *
+   * @link https://codex.wordpress.org/Post_Thumbnails
+   */
+
+  add_theme_support( 'post_thumbnails' );
+
 }
+
 add_action( 'after_setup_theme', 'perseowiki_support' );
 
 function perseowiki_nav_menus() {
   
   // This theme uses wp_nav_menu() in one location.
+  
   register_nav_menus([
-    'really-simple-primary-menu' => esc_html__( 'Primary Menu', 'perseowiki' ),
+    'perseowiki-primary-menu' => esc_html__( 'Primary Menu', 'perseowiki' ),
   ]);
 }
+
 add_action( 'init', 'perseowiki_nav_menus' );
 
 ?>
