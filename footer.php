04d79e2146805d3f1376fb179f6a5b9df17b535c
giovannimanetti11 <giovanni@perseodesign.com>
2023-03-18 12:16:31 +0000

    Add Privacy and Disclaimer to footer

--- footer.php
+++ footer.php
@@ -4,6 +4,12 @@
                 <div class="footerBottom">
                     &copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?>
                 </div>
+                <div class="footerLinks">
+                    <ul>
+                        <a href="https://www.wikiherbalist.com/privacy-policy/"><li>Privacy Policy</li></a> | 
+                        <a href="https://www.wikiherbalist.com/diclaimer/"><li>Disclaimer</li></a>
+                    </ul>
+                </div>
             </div>
         </div>
     </footer>
--- page.php
+++ page.php
@@ -1,25 +1,25 @@
 <?php get_header(); ?>
 
-<main class="container">
-    <?php 
-    
-    if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <main id="main" class="site-main" role="main">
+                <?php while (have_posts()) : the_post(); ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <div class="entry-header">
+                            <h1 class="entry-title"><?php the_title(); ?></h1>
+                        </div>
 
-    <article>
+                        <div class="entry-content">
+                            <?php the_content(); ?>
+                        </div>
 
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
 
-    </article>
-
-    <?php endwhile;
-
-    else :
-        _e( 'Sorry, no posts were found.', 'textdomain' );
-    endif;
-
-    ?>
-
-</main>
+                    </article>
+                <?php endwhile; ?>
+            </main>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
