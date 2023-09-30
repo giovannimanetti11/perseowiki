afee591955c80c21bb63d455904de13e997021b9
giovannimanetti11 <giovanni@perseodesign.com>
2023-09-30 17:35:12 +0000

    Added voice speech on glossary pages

--- single-termine.php
+++ single-termine.php
@@ -22,7 +22,9 @@
             </div>
         </div>
 
-        <h1><?php the_title(); ?></h1>
+        <div class="article-title">
+            <h1><?php the_title(); ?></h1> <i class="fa-solid fa-volume-up" data-text="<?php echo esc_attr(get_the_title()); ?>" data-language="it"></i>
+        </div>
 
         <?php $meta_box_value = get_post_meta( get_the_ID(), 'meta-box-nome-scientifico', true ); ?>
         <?php if (!empty($meta_box_value)) : ?>
