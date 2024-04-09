c8a78815ffe14bfdce47b2281909f8f83a94c64e
giovannimanetti11 <giovanni@perseodesign.com>
2024-04-09 10:51:37 +0000

    Update Twitter icon with X in blog post share popup

--- single-blog.php
+++ single-blog.php
@@ -48,7 +48,7 @@
                     <span>Condividi sui social</span>
                     <div class="social-icons">
                         <a href="#" class="social-icon facebook" onclick="shareUrl('https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()); ?>')"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="social-icon twitter" onclick="shareUrl('https://twitter.com/intent/tweet?url=<?php echo urlencode(get_permalink()); ?>')"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#" class="social-icon twitter" onclick="shareUrl('https://twitter.com/intent/tweet?url=<?php echo urlencode(get_permalink()); ?>')"><i class="fa-brands fa-x-twitter"></i></a>
                         <a href="#" class="social-icon linkedin" onclick="shareUrl('https://www.linkedin.com/shareArticle?url=<?php echo urlencode(get_permalink()); ?>')"><i class="fa-brands fa-linkedin" aria-hidden="true"></i></a>
                         <a href="https://api.whatsapp.com/send?text=<?php echo urlencode(get_the_title() . ' ' . get_permalink()); ?>" class="social-icon whatsapp" onclick="return shareUrl(this.href);"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
                         <a href="#" class="social-icon telegram" onclick="shareUrl('https://telegram.me/share/url?url=<?php echo urlencode(get_permalink()); ?>&text=<?php echo urlencode(get_the_title()); ?>')"><i class="fab fa-telegram" aria-hidden="true"></i></a>
